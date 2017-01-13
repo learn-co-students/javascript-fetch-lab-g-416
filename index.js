d9c91c00cf09caaa615c4f44643d03c1f911aaff
andrew <github email address>
2017-01-13 08:53:54 +0200

    partial change

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+
+  fetch(repo)
+    .then(res => res.json())
+    .then(json => showResults(json));
 }
 
 function getToken() {
