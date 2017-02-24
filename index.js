ed5f291efd7d064ece13213d30d9583f14b5ee2e
abadfish <kirsten.ofarrell@gmail.com>
2017-02-24 15:28:29 -0500

    need html_url in issues-template in index.html and test

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
+const fork = `abadfish/javascript-fetch-lab`
+
 function getIssues() {
+  fetch('https://api.github.com/repos/' + fork + '/issues')
+    .then(resp => resp.json())
+    .then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  const template = Handlebars.compile(document.getElementById('issues-template').innerHTML)
+  document.getElementById('issues').innerHTML = template(json)
 }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+
+  fetch('https://api.github.com/repos/' + fork + '/issues', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
+  debugger;
 }
 
 function showResults(json) {
+  const template = Handlebars.compile(document.getElementById('repo-template').innerHTML)
+  document.getElementById('results').innerHTML = template(json)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const token = getToken();
+  fetch('https://api.github.com/repos/' + repo + '/forks', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
